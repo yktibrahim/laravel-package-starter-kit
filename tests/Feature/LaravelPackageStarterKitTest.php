0fecb63e2252656fb0daece2eb54ebd226699ac8
ibrahim <yktibrahim@gmail.com>
2025-05-05 00:11:50 +0300

    bug fixed for test

--- tests/Feature/LaravelPackageStarterKitTest.php
+++ tests/Feature/LaravelPackageStarterKitTest.php
@@ -7,9 +7,41 @@ use LaravelPackageStarterKit\LaravelPackageStarterKitServiceProvider;
 
 class LaravelPackageStarterKitTest extends TestCase
 {
+    /**
+     * Get package providers.
+     * Paket sağlayıcılarını al.
+     * 
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
     protected function getPackageProviders($app)
     {
         return [LaravelPackageStarterKitServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     * Çevre kurulumunu tanımla.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Setup default database to use sqlite :memory:
+        // Varsayılan veritabanını sqlite :memory: kullanacak şekilde ayarla
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+
+        // Set up encryption key
+        // Şifreleme anahtarını ayarla
+        $app['config']->set('app.key', 'base64:'.base64_encode(
+            '01234567890123456789012345678901'
+        ));
     }
 
     /**
--- tests/Unit/LaravelPackageStarterKitUnitTest.php
+++ tests/Unit/LaravelPackageStarterKitUnitTest.php
@@ -7,9 +7,37 @@ use LaravelPackageStarterKit\LaravelPackageStarterKitServiceProvider;
 
 class LaravelPackageStarterKitUnitTest extends TestCase
 {
+    /**
+     * Get package providers.
+     * Paket sağlayıcılarını al.
+     * 
+     * @param \Illuminate\Foundation\Application $app
+     * @return array
+     */
     protected function getPackageProviders($app)
     {
         return [LaravelPackageStarterKitServiceProvider::class];
+    }
+
+    /**
+     * Define environment setup.
+     * Çevre kurulumunu tanımla.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     * @return void
+     */
+    protected function getEnvironmentSetUp($app)
+    {
+        // Set up encryption key
+        // Şifreleme anahtarını ayarla
+        $app['config']->set('app.key', 'base64:'.base64_encode(
+            '01234567890123456789012345678901'
+        ));
+
+        // Set the package configuration
+        // Paket yapılandırmasını ayarla
+        $app['config']->set('laravelpackagestarterkit.name', 'Laravel Package Starter Kit');
+        $app['config']->set('laravelpackagestarterkit.enabled', true);
     }
 
     /**
